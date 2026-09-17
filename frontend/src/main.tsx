a0137f996c033626186d441723838cff0c2085f4
Shubham5362 <07061996sy@gmail.com>
2026-09-17 20:30:49 +0530

    revert: remove uploaded frontend files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles.css';
+import './ai-chat-fullscreen.css';
+import SellerHubApp from './SellerHubApp';
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SellerHubApp />
+  </React.StrictMode>,
+);
